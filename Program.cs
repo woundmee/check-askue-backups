2fa0b6ff9bef9eb3041c9e48dab2577574e99c17
woundmee <woundmee@gmail.com>
2025-07-22 10:52:16 +0700

    добавлены аргументы для работы со скриптом из терминала.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,59 @@
-﻿using CheckBackups.Services;
+﻿// dev: RadzhabovRI
+// github: @woundmee
+// last update: 22.07.2025
+// version: 2.0
+
+using CheckBackups.Services;
 
 namespace CheckBackups;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        // Database backup
-        var dbBackups = new CheckDatabaseBackups();
-        var dbCheck = dbBackups.Check("dbBackupsFolder");
-        Console.WriteLine(dbCheck);
-        
-        // OS backup
-        var osBackup = new CheckOsBackups();
-        var osCheck = osBackup.Check("osBackupsFolder", "");
-        Console.WriteLine(osCheck);
+        if (args.Length == 0)
+            ConsoleActions.GetHelper();
+        else
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-h":
+                        ConsoleActions.GetHelper();
+                        break;
+                    case "-s":
+                        var dbBackups = new CheckDatabaseBackups();
+                        var dbCheck = dbBackups.Check(args[i + 1]);
+                        break;
+                    case "-v":
+                        var osBackup = new CheckOsBackups();
+                        var osCheck = osBackup.Check(args[i + 1], args[i + 2]);
+                        break;
+                    case "-i":
+                        ConsoleActions.GetAdditionalInfo();
+                        break;
+                    // default:
+                    //     Console.WriteLine("Аргументы не заданы или заданы некорректно.");
+                    //     break;
+                }
+            }
+        }
 
-        Console.WriteLine();
-        
-        Helper.GetHelper();
+
+        // ============
+        //     // Database backup
+        // var dbBackups = new CheckDatabaseBackups();
+        // var dbCheck = dbBackups.Check("dbBackupsFolder");
+        // Console.WriteLine(dbCheck);
+        //
+        //     // OS backup
+        // var osBackup = new CheckOsBackups();
+        // var osCheck = osBackup.Check("osBackupsFolder", "");
+        // Console.WriteLine(osCheck);
+        //
+        //     Console.WriteLine();
+        //     
+        //     Helper.GetHelper();
     }
 }
-
---
+++ Services/ConsoleActions.cs
@@ -0,0 +1,27 @@
+﻿namespace CheckBackups.Services;
+
+public class ConsoleActions
+{
+    internal static void GetHelper()
+    {
+        Console.WriteLine("\n================================================");
+        Console.WriteLine("# Проверка SQL/Veeam бэкапов на серверах АСКУЭ #");
+        Console.WriteLine("================================================\n");
+        Console.WriteLine("Доступные аргументы:\n");
+        Console.WriteLine("-h\tСправка");
+        Console.WriteLine("-s\tПроверка SQL-бэкапов");
+        Console.WriteLine("-v\tПроверка Veeam-бэкапов");
+        Console.WriteLine("-i\tДоп. информация\n");
+    }
+
+    internal static void GetAdditionalInfo()
+    {
+        Console.WriteLine("\n- Dev: RadzhabovRI");
+        Console.WriteLine("- Github: @woundmee");
+        Console.WriteLine("- Version: 2.0\n");
+        Console.WriteLine("\nКак пользоваться скриптом?");
+        Console.WriteLine("Подробнее расписано в readme.md. Команды вкратце ниже.");
+        Console.WriteLine("\n- SQL: \"./CheckBackups -s Folder\", где Folder - это каталог с подкаталогами Day, Month и Year");
+        Console.WriteLine("- Veeam: \"./CheckBackups -v Folder\", где Folder - это каталог с бэкап-файлами veeam\n");
+    }
+}
